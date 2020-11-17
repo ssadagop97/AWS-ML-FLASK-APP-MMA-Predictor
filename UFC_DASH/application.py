9441e3994a33d6f78215d942b5f04ef36ea6ef0c
ssadagop97 <61854724+ssadagop97@users.noreply.github.com>
2020-11-17 01:38:06 -0500

    Update application.py

--- UFC_DASH/application.py
+++ UFC_DASH/application.py
@@ -571,7 +571,7 @@ def set_image_f1(fighter1):
         fighter1 = 'Aleksei Oliynyk'
         
     #return get_fighter_url(fighter1)
-    return "https://github.com/ssadagop97/UFC_DASH/tree/main/UFC_DASH/Pictures/fighter_left.jpg?raw=true"
+    return "https://github.com/ssadagop97/UFC_DASH/tree/main/UFC_DASH/Pictures/fighter_left.png?raw=true"
 
 
 @application.callback(
@@ -584,7 +584,7 @@ def set_image_f2(fighter2):
         
     #return get_fighter_url(fighter2)
     
-    return "https://github.com/ssadagop97/UFC_DASH/tree/main/UFC_DASH/Pictures/fighter_right.jpg?raw=true"
+    return "https://github.com/ssadagop97/UFC_DASH/tree/main/UFC_DASH/Pictures/fighter_right.png?raw=true"
 
 
 @application.callback(
